18b2c4224f1e3a58535d45023e6c59f7403c1d8a
name = andeuw <66847734+andeuw@users.noreply.github.com>
2024-11-24 17:16:37 +0100

    Sprungmarke zum abbrechen eines switch-case

--- src/doubleProblems.java
+++ src/doubleProblems.java
@@ -20,5 +20,38 @@ public class doubleProblems {
             // System.out.println( "hell" );
         }
         System.out.println( "heaven" );
+
+        System.out.println();
+        // Beispiel: http: als Sprungmarke und //www.tutego.de/ als Kommentar
+        http://www.tutego.de/
+        System.out.print( "Da gibt's Java-Tipps und -Tricks." );
+
+        System.out.println("\n################################################################\n");
+        System.out.println("--- Abbruch eines for-Loops im default-Teil eines switch-case durch break und Sprungmarke ---\n");
+
+        String dnaBases = "CGCAGTTCTTCGGXAC";
+        int a = 0, g = 0, c = 0, t = 0;
+
+        loop:
+        for ( int i = 0; i < dnaBases.length(); i++ ) {
+            switch ( dnaBases.charAt( i ) ) {
+                case 'A': case 'a':
+                    a++;
+                    break;
+                case 'G': case 'g':
+                    g++;
+                    break;
+                case 'C': case 'c':
+                    c++;
+                    break;
+                case 'T': case 't':
+                    t++;
+                    break;
+                default:
+                    System.err.println( "Unbekannte Nukleinbasen " + dnaBases.charAt( i ) );
+                    break loop;
+            }
+        }
+        System.out.printf( "Anzahl: A=%d, G=%d, C=%d, T=%d%n", a, g, c, t );
     }
 }
